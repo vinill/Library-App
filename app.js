ae3d829af729ccc82dee0924b0259ce43be1f3cb
vinilll <dzisvolodymyr@gmail.com>
2018-02-20 23:52:47 +0100

    Finish configuration of project: 1.Adding Bower 2.Adding Gulp

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 var express = require('express');
 var app = express();
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
+app.use(express.static('source/views'));
 
 app.get('/', function (req, res) {
     res.send('Hello world');
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,50 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 
-var jsfiles = ['*.js', 'source/**/*.js'];
+var jsFiles = ['*.js', 'source/**/*.js'];
+
 gulp.task('style', function () {
 
-    gulp.src(jsfiles)
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
         }));
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    var injectOpions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./source/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOpions))
+        .pipe(gulp.dest('./source/views'));
+});
+
+gulp.task('serve',['style', 'inject'],function () {
+   var options = {
+       script: 'app.js',
+       delayTime: 1,
+       env: {
+           'PORT': 3000
+       },
+       watch: jsFiles
+   };
+   return nodemon(options)
+       .on('restart', function (ev) {
+           console.log('Restarting.....');
+       });
 });
