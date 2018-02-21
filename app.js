a89ec20881eb31392e52ebaa16cfd52b7c53eea1
vinilll <dzisvolodymyr@gmail.com>
2018-02-22 00:59:16 +0100

    Routing works

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
@@ -9,7 +8,15 @@ app.set('views', './source/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index',{title: 'Title'});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'}]
+    });
 });
 
 app.listen(port, function (err) {
