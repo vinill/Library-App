5c173b5ebdc42121ecfdc98f81101cad4ed57a0e
vinilll <dzisvolodymyr@gmail.com>
2018-03-10 00:42:53 +0100

    Add sign up form with all backend stuff

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parers');
+var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var session = require('express-session');
 
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(session({sercret: 'library'}));
+app.use(session({secret: 'library'}));
 
 require('./source/config/passport')(app);
 
--- source/routes/authRoutes.js
+++ source/routes/authRoutes.js
@@ -6,6 +6,26 @@ var router = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
         console.log(req.body);
+            var url =
+                'mongodb://localhost:27017';
+
+            mongodb.connect(url, function (err, client) {
+                var collection = client.db('libraryApp').collection('users');
+                var user = {
+                    username: req.body.userName,
+                    password: req.body.password
+                };
+                collection.insert(user,
+                    function (err, results) {
+                        req.login(results.ops[0], function () {
+                            res.redirect('/auth/profile');
+                        });
+                    });
+            });
+        });
+    authRouter.route('/profile')
+        .get(function (req, res) {
+            res.json(req.user);
         });
     return authRouter;
 };
